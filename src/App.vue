8e8b98093dec3910fe7dd8aa789efdad4c30d8f3
mekanio20 <mrxyok138@gmail.com>
2024-11-07 10:02:47 +0500

    loader update

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 <script>
 import { RouterView } from 'vue-router'
 import Loader from "@/components/Loader.vue";
+
 export default {
   name: "App",
   components: {
@@ -30,12 +31,34 @@ export default {
   methods: {
     handleRouteChange() {
       this.loading = true;
+
       this.$nextTick(() => {
+        const images = Array.from(document.images);
+
+        if (images.length === 0) {
+          this.loading = false;
+          return;
+        }
+
         setTimeout(() => {
           this.loading = false;
         }, 7000);
+
+        // const imagePromises = images.map((img) =>
+        //   img.complete
+        //     ? Promise.resolve()
+        //     : new Promise((resolve) => {
+        //       img.onload = resolve;
+        //       img.onerror = resolve;
+        //     })
+        // );
+
+        // Promise.all(imagePromises).then(() => {
+        //   this.loading = false;
+        //   clearTimeout(this.loadTimeout)
+        // });
       });
     },
   },
-}
+};
 </script>
