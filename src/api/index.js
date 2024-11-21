2601cb89e4c4af4fc2f9a0bb0301649767001a54
mekanio20 <mrxyok138@gmail.com>
2024-11-21 20:40:14 +0500

    mixed content

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,16 @@
 import axios from "axios"
-export default axios.create({
+const instance = axios.create({
     baseURL: "http://bmamedov.beget.tech/api",
     withCredentials: true,
 })
+
+instance.interceptors.request.use((config) => {
+    config.headers['Access-Control-Allow-Origin'] = '*'; // CORS için izin
+    config.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'; // Yöntemlere izin
+    config.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'; // Başlıklara izin
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+export default instance;
