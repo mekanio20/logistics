e3546a0926aadbb4646f0698a69ee4f3cb0ca768
mekanio20 <mrxyok138@gmail.com>
2024-12-02 17:43:38 +0500

    last commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,5 @@
 import axios from "axios"
-const instance = axios.create({
+export default axios.create({
     baseURL: "http://bmamedov.beget.tech/api",
     withCredentials: true,
 })
-
-instance.interceptors.request.use((config) => {
-    config.headers['Access-Control-Allow-Origin'] = '*'; // CORS için izin
-    config.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'; // Yöntemlere izin
-    config.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'; // Başlıklara izin
-    return config;
-}, (error) => {
-    return Promise.reject(error);
-});
-
-export default instance;
